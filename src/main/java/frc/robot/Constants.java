394b4661d878d1e0614ea3006be64efc55a7cc2c
AT727 <atxponh@gmail.com>
2023-09-27 23:08:56 -0400

    W RAF PRETTY CODE

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,24 +18,25 @@ public final class Constants {
   }
 
   public static class Arm {
-    public static class Anchor{
+    public static class Anchor {
       public static final int kAnchorPort = 0;
       public static final int kAnchorEncoderPort = 0;
       public static final boolean kInverted = false;
-      public static final int kCurrentLimit = 60; //40 - 60
+      public static final int kCurrentLimit = 60; // 40 - 60
       public static final double kGearRatio = 1;
 
       /*
-      velocity from encoder is rotations/s 
+      velocity from encoder is rotations/s
       rotations/s * (meters/rotations) = meters/s
       m/s what we need for motion profile
+
       */
       public static final double kdistancePerRotation = 0;
 
       public static final double kP = 0;
       public static final double kI = 0;
       public static final double kD = 0;
-  
+
       public static final double ks = 0;
       public static final double kg = 0;
       public static final double kv = 0;
@@ -44,9 +45,8 @@ public final class Constants {
 
       public static final double maxVelocity = 0;
       public static final double maxAcceleration = 0;
-      public static final TrapezoidProfile.Constraints ANCHOR_CONSTRAINTS = new TrapezoidProfile.Constraints(maxVelocity, maxAcceleration);
-
- 
+      public static final TrapezoidProfile.Constraints ANCHOR_CONSTRAINTS =
+          new TrapezoidProfile.Constraints(maxVelocity, maxAcceleration);
 
       public static final double kMinAngle = Double.NEGATIVE_INFINITY;
       public static final double kMaxAngle = Double.POSITIVE_INFINITY;
@@ -57,18 +57,18 @@ public final class Constants {
       public static final double kMinOutput = Double.NEGATIVE_INFINITY;
     }
 
-    public static class Floating{
+    public static class Floating {
       public static final int kFloatingPort = 0;
       public static final int kFloatingEncoderPort = 0;
       public static final boolean kInverted = false;
-      public static final int kCurrentLimit = 60; //40 - 60
+      public static final int kCurrentLimit = 60; // 40 - 60
       public static final double kGearRatio = 1;
-      public static final double kdistancePerRotation = 0; 
+      public static final double kdistancePerRotation = 0;
 
       public static final double kP = 0;
       public static final double kI = 0;
       public static final double kD = 0;
-  
+
       public static final double ks = 0;
       public static final double kg = 0;
       public static final double kv = 0;
@@ -77,21 +77,16 @@ public final class Constants {
 
       public static final double maxVelocity = 0;
       public static final double maxAcceleration = 0;
-      public static final TrapezoidProfile.Constraints FLOATING_CONSTRAINTS = new TrapezoidProfile.Constraints(maxVelocity, maxAcceleration);
-  
+      public static final TrapezoidProfile.Constraints FLOATING_CONSTRAINTS =
+          new TrapezoidProfile.Constraints(maxVelocity, maxAcceleration);
+
       public static final double kMinAngle = Double.NEGATIVE_INFINITY;
       public static final double kMaxAngle = Double.POSITIVE_INFINITY;
       public static final boolean kEnableSoftLimit = false;
       public static final double kZeroPosition = 0;
 
-
       public static final double kMaxOutput = Double.POSITIVE_INFINITY;
       public static final double kMinOutput = Double.NEGATIVE_INFINITY;
-
-
     }
-   
-    
-
   }
 }
