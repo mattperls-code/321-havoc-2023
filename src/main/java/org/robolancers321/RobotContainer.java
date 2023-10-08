87ef111fb04060e5fe02ea404c62e1d543ebe1c3
AT727 <atxponh@gmail.com>
2023-10-08 13:55:42 -0400

    commits I didn't stage

--- src/main/java/org/robolancers321/RobotContainer.java
+++ src/main/java/org/robolancers321/RobotContainer.java
@@ -4,11 +4,9 @@ package org.robolancers321;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import java.io.FileNotFoundException;
-import org.robolancers321.commands.Autos;
-import org.robolancers321.commands.ExampleCommand;
-import org.robolancers321.subsystems.ExampleSubsystem;
+import org.robolancers321.subsystems.arm.Arm;
+import org.robolancers321.subsystems.arm.commands.RunArm;
 import org.robolancers321.util.VersionLoader;
 
 /**
@@ -18,19 +16,14 @@ import org.robolancers321.util.VersionLoader;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
+  private Arm arm = new Arm();
+  private final CommandXboxController controller =
       new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
+    this.arm.setDefaultCommand(new RunArm(arm));
     configureBindings();
-
-    // Display versioning info on SmartDashboard
 
     try {
       String versionData = new VersionLoader().getVersionData().toString();
@@ -43,32 +36,9 @@ public class RobotContainer {
     }
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+  private void configureBindings() {}
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
