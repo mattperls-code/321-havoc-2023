9f9b03245d5e396b524b9f81bc2e73bba0e99ff2
AT727 <atxponh@gmail.com>
2023-10-08 13:55:25 -0400

    fixed conversions, motion profile, arm feedforward, implemented default cmd & inv kinematics

---
+++ src/main/java/org/robolancers321/subsystems/arm/commands/RunArm.java
@@ -0,0 +1,74 @@
+/* (C) Robolancers 2024 */
+package org.robolancers321.subsystems.arm.commands;
+
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.SparkMaxPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.robolancers321.Constants;
+import org.robolancers321.subsystems.arm.Arm;
+
+public class RunArm extends CommandBase {
+
+  Arm arm;
+
+  public RunArm(Arm arm) {
+    this.arm = arm;
+
+    addRequirements(arm);
+  }
+
+  @Override
+  public void execute() {
+    // //cal FF
+    // arm.periodicIO.anchorFF =
+    //     Constants.Arm.Anchor.FF.ANCHOR_FEEDFORWARD.calculate(
+    //         Math.toRadians(arm.periodicIO.anchorPosSetpoint), 0);
+    // arm.periodicIO.floatingFF =
+    //     Constants.Arm.Floating.FF.FLOATING_FEEDFORWARD.calculate(
+    //         Math.toRadians(arm.periodicIO.floatingPosSetpoint), 0);
+
+    // // set FF and setpoint
+    // arm.anchorPIDController.setReference(
+    //     arm.periodicIO.anchorPosSetpoint,
+    //     ControlType.kPosition,
+    //     Constants.Arm.Anchor.PID.kSlot,
+    //     arm.periodicIO.anchorFF,
+    //     SparkMaxPIDController.ArbFFUnits.kVoltage);
+    // arm.floatingPIDController.setReference(
+    //     arm.periodicIO.floatingPosSetpoint,
+    //     ControlType.kPosition,
+    //     Constants.Arm.Floating.PID.kSlot,
+    //     arm.periodicIO.floatingFF,
+    //     SparkMaxPIDController.ArbFFUnits.kVoltage);
+
+    TrapezoidProfile.State anchorProfileState =
+        arm.periodicIO.anchorProfile.calculate(
+            Timer.getFPGATimestamp() - arm.periodicIO.anchorProfileStartTime);
+
+    TrapezoidProfile.State floatingProfileState =
+        arm.periodicIO.floatingProfile.calculate(
+            Timer.getFPGATimestamp() - arm.periodicIO.floatingProfileStartTime);
+
+    arm.periodicIO.anchorFF =
+        Constants.Arm.Anchor.FF.ANCHOR_FEEDFORWARD.calculate(
+            Math.toRadians(anchorProfileState.position), anchorProfileState.velocity);
+    arm.periodicIO.floatingFF =
+        Constants.Arm.Floating.FF.FLOATING_FEEDFORWARD.calculate(
+            Math.toRadians(floatingProfileState.position), floatingProfileState.velocity);
+
+    arm.floatingPIDController.setReference(
+        floatingProfileState.position,
+        ControlType.kPosition,
+        Constants.Arm.Floating.PID.kSlot,
+        arm.periodicIO.floatingFF,
+        SparkMaxPIDController.ArbFFUnits.kVoltage);
+    arm.anchorPIDController.setReference(
+        anchorProfileState.position,
+        ControlType.kPosition,
+        Constants.Arm.Anchor.PID.kSlot,
+        arm.periodicIO.anchorFF,
+        SparkMaxPIDController.ArbFFUnits.kVoltage);
+  }
+}
